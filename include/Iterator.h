c807932717f6650c500ff92d61b2ea07fb2f635a
Artem <a.danylov39@gmail.com>
2024-05-02 22:10:07 +0300

    Complete version 1.0.0
    Add public include headers, CMakeLists.txt and CMake config resource file

---
+++ include/Iterator.h
@@ -0,0 +1,47 @@
+#ifndef CSTL_ITERATOR_H
+#define CSTL_ITERATOR_H
+
+#include "BeginCode.h"
+
+#include <CSTL_Exception/Exception.h>
+#include <CSTL_SmartPointer/SmartPointer.h>
+
+typedef struct CSTL_Iterator {
+    void *container;
+    void *element;
+} CSTL_Iterator;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef CSTL_GetIteratorContainer
+# define CSTL_GetIteratorContainer(CT, IT) (CT *)_CSTL_GetIteratorContainer(IT)
+#endif /* CSTL_GetIteratorContainer */
+
+#ifndef CSTL_GetIteratorData
+# define CSTL_GetIteratorData(CT, IT) ((CT##Element *)((Iterator *)IT)->element)->data
+#endif /* CSTL_GetIteratorData */
+
+#ifndef CSTL_SetIteratorData
+# define CSTL_SetIteratorData(CT, IT, DATA) ((CT##Element *)((Iterator *)IT)->element)->data = DATA
+#endif /* CSTL_SetIteratorData */
+
+#ifndef CSTL_IsIteratorEquals
+# define CSTL_IsIteratorEquals(CT, IT1, IT2) \
+    CSTL_GetIteratorData(CT, IT1) == CSTL_GetIteratorData(CT, IT2) ? CSTL_TRUE : CSTL_FALSE
+#endif /* CSTL_IsIteratorEquals */
+
+#ifndef CSTL_IterateForward
+# define CSTL_IterateForward(CT, IT) _CSTL_FDECL_Iterator_##CT##_iterateForward(IT)
+#endif /* CSTL_IterateForward */
+
+#ifndef CSTL_IterateBack
+# define CSTL_IterateBack(CT, IT) _CSTL_FDECL_Iterator_##CT##_iterateBack(IT)
+#endif /* CSTL_IterateBack */
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* CSTL_ITERATOR_H */
