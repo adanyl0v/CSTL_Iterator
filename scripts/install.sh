ba43f7c4498d71d128166a126384ffaf9c8e720b
Artem <a.danylov39@gmail.com>
2024-05-05 08:19:41 +0300

    Fix install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 read -p "Enter the installation config: " CONFIG
-
-# Comment next line to keep installed other library versions
-rm -rf /usr/local/lib/libCSTL_Exception.so*
-
 cmake --install "build/$CONFIG" --config $CONFIG
